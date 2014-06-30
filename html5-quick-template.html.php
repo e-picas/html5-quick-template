8622186daf41a4e01998cd6dcee099e5a00059d2
piwi <me@e-piwi.fr>
2014-06-30 07:46:47 +0200

    preparing version tag 1.2.0

--- html5-quick-template.html.php
+++ html5-quick-template.html.php
@@ -355,7 +355,7 @@ define('HQT_NAME', 'html5-quick-template');
 /**
  * @constant    Current version of the app
  */
-define('HQT_VERSION', '1.1.2');
+define('HQT_VERSION', '1.2.0');
 
 /**
  * @constant    URL of the app repo
