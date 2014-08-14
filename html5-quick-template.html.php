639d53b6219dd6f2f8229045716f528dc414e16c
piwi <me@e-piwi.fr>
2014-08-14 22:31:56 +0200

    upgrading to version 1.2.2

--- html5-quick-template.html.php
+++ html5-quick-template.html.php
@@ -364,7 +364,7 @@ define('HQT_NAME', 'html5-quick-template');
 /**
  * @constant    Current version of the app
  */
-define('HQT_VERSION', '1.2.1');
+define('HQT_VERSION', '1.2.2');
 
 /**
  * @constant    URL of the app repo
