28ecc1e247fdc76a9514a187622f21858b92dea8
piwi <me@e-piwi.fr>
2014-10-18 12:31:27 +0200

    pushing version number to 1.2.6-dev following release of 1.2.5

--- html5-quick-template.html.php
+++ html5-quick-template.html.php
@@ -371,7 +371,7 @@ define('HQT_NAME', 'html5-quick-template');
 /**
  * @constant    Current version of the app
  */
-define('HQT_VERSION', '1.2.5-dev');
+define('HQT_VERSION', '1.2.6-dev');
 
 /**
  * @constant    URL of the app repo
